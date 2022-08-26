ec8f9591e744b32d5fbfe53e4256fb44803bdd51
Simion <simion.okola@student.moringaschool.com>
2022-08-26 18:01:50 +0300

    Completed assignment

--- test/index.js
+++ test/index.js
@@ -1,21 +1,47 @@
-const chai = require("chai");
-global.expect = chai.expect;
-const isPalindrome = require("../index");
+function isPalindrome(word) {
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+    const endIndex = word.length - 1 - startIndex;
+    if (word[startIndex] !== word[endIndex]) {
+      return false;
+    }
+  }
 
-describe("isPalindrome", () => {
-  it("returns true for 'abba'", () => {
-    expect(isPalindrome("abba")).to.be.true;
-  });
-  it("returns true for 'racecar'", () => {
-    expect(isPalindrome("racecar")).to.be.true;
-  });
-  it("returns true for 'a'", () => {
-    expect(isPalindrome("a")).to.be.true;
-  });
-  it("returns false for 'robot'", () => {
-    expect(isPalindrome("robot")).to.be.false;
-  });
-  it("returns false for 'ab'", () => {
-    expect(isPalindrome("ab")).to.be.false;
-  });
-});
+  return true;
+}
+
+
+//pseudocode
+// iterate from the beginning of the string to the middle of the string
+  // compare the letter we're iterating over to the corresponding letter at the end of the string
+  // if the letters don't match, return false
+  // if we reach the middle, and all the letters match, return true
+
+if (require.main === module) {
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+}
